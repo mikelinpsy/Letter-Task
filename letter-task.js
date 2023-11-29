d9405b3e7e317dd1155d23c3bb4128e0ae612609
Mike Lin <151608135+mikelinpsy@users.noreply.github.com>
2023-11-29 04:56:12 -0600

    Update letter-task.js

--- letter-task.js
+++ letter-task.js
@@ -170,9 +170,9 @@ var debrief_block = {
         var log_global_rt = Math.round(Math.log(valid_global_trials.select('rt')).mean());
         var log_local_rt = Math.round(Math.log(valid_local_trials.select('rt')).mean());
         
-        return "<p>You responded correctly on " + accuracy + "% of the trials."+ rtvalid + "1" + valid +"</p>" +
-            "<p>Your average response time was " + rt + "e" + global_rt + "e"  +  local_rt + "ms.</p>" +
-            "<p>Press any key to complete the experiment. "+ log_global_rt + "e"  + log_local_rt+ "Thank you!  </p>";
+        return "<p>You responded correctly on " + accuracy + "% of the trials.</p>" +
+            "<p>Your average response time was " + rt + "ms.</p>" +
+            "<p>Press any key to complete the experiment. Thank you!</p>";
 
 
     }
