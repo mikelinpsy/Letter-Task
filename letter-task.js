773621aa25338877119a7b911b574b1280c59e24
Mike Lin <151608135+mikelinpsy@users.noreply.github.com>
2023-11-29 05:26:34 -0600

    Update letter-task.js

--- letter-task.js
+++ letter-task.js
@@ -120,7 +120,7 @@ var test = {
     data: jsPsych.timelineVariable('data'),
     on_finish: function (data) {
         data.correct = data.key_press == jsPsych.pluginAPI.convertKeyCharacterToKeyCode(data.correct_response);
-        data.logrt = Math.log(data.rt);
+
     },
 }
 
@@ -168,6 +168,8 @@ var debrief_block = {
         var rt = Math.round(valid_trials.select('rt').mean());
         var global_rt = Math.round(valid_global_trials.select('rt').mean());
         var local_rt = Math.round(valid_local_trials.select('rt').mean());
+        var log_global_rt = Math.round(valid_global_trials.logrt).mean());
+        var log_local_rt = Math.round(valid_local_trials.logrt).mean())
 
 
         return "<p>You responded correctly on " + accuracy + "% of the trials.</p>" +
